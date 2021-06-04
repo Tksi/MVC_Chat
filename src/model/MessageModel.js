dabd686e776b1e0a6b6a818cce3ce4d9ee9be64a
Tksi <kanatama7@gmail.com>
2021-06-04 15:16:15 +0900

    [upd] addMessageされた時、条件が合えばws.send()するようにした

--- src/model/MessageModel.js
+++ src/model/MessageModel.js
@@ -1,6 +1,6 @@
 export const MessageModel = class {
-  constructor({ ws, messages = [] }) {
-    this.mesagges = messages;
+  constructor(ws) {
+    this.mesagges = [];
     this.ws = ws;
   }
 
@@ -10,9 +10,12 @@ export const MessageModel = class {
    * @param {{ body: string, option: string, from: string, isBroad: Boolean, to: string }} isSendがtrueの場合は送信用
    */
   addMessage = ({ body, option, from, isBroad, to }) => {
-    this.mesagges.push({ body, option, from, isBroad, to });
-    //TODO: toがあればws.sendする
-    //TODO: viewからelement取得する
+    this.mesagges.push({ body, option: '', from: '', isBroad: '', to: '' });
+    //toがあればws.sendする
+    if (to) {
+      this.ws.send(JSON.stringify({ body, to, option }));
+    }
+    //TODO: viewからelement取得する -> Serverからのやつはわけたり
     //TODO: 描画する(render)
   };
 };
