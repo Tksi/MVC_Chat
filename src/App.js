858e97b2510e1d2ae26d47f9dec593a3eca33dd7
Tksi <kanatama7@gmail.com>
2021-06-04 12:08:54 +0900

    [upd] MessagaModelを使う雛形

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { MessageModel } from './model/MessageModel.js';
+
 export const App = class {
   constructor({ jsForm, jsMessages, wsURL }) {
     this.jsForm = jsForm;
     this.jsMessages = jsMessages;
     this.ws = new WebSocket(wsURL);
+    this.messageModel = new MessageModel();
   }
 
   //イベントリスナーの登録
@@ -11,11 +14,14 @@ export const App = class {
       e.preventDefault();
       console.log(e.srcElement.input.value);
       //TODO: MessagaModelに値を追加する
+      // this.messageModel.addMessage();
+      e.srcElement.input.value = '';
     });
 
     this.ws.addEventListener('message', (message) => {
       console.log(message);
       //TODO: MessageModelに値を追加する
+      // this.messageModel.addMessage();
     });
   }
 };
